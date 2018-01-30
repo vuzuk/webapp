a710a059217618fd85d3eddd040d814f9ca7ab1d
varunzxzx <varun995862@gmail.com>
2018-01-30 18:34:48 +0530

    fix minor changes

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-// const passport = require('passport');
-// const passportLocal = require('passport-local');
-// const session = require('express-session');
 const path = require('path');
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
-const dotenv = require('dotenv');
-
-require('dotenv').config();
-dotenv.load();
-
-const routes = {
-    // secure: require('./routes/secure'),
-    // insecure: require('./routes/insecure'),
-    index : require('./routes/index'),
-    users : require('./routes/users'),
-};
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-
-app.use('/public', express.static(path.join(__dirname, 'public')));
-// app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes.index);
-app.use('/users', routes.users);
+const render = require('./dist/SSR');
+
+if(process.env.NODE_ENV !== 'PRODUCTION') {
+  console.log("In Development Environment");
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.dev-client');
+  const compiler = webpack(webpackConfig);
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath,
+    log: console.log
+  }));
+  app.use(require("webpack-hot-middleware")(compiler));
+} else {
+  console.log("PRODUCTION ENVIRONMENT");
+  app.use(express.static('dist'));
+}
+
+app.get('/',render.default);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.send('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
 });
 
 module.exports = app;
