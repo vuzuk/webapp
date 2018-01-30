6235615047aaaec3d05c49de9ea1d68831f4fa72
varunzxzx <varun995862@gmail.com>
2018-01-30 17:29:03 +0530

    removed routes

--- src/SSR.js
+++ src/SSR.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import template from './template';
-import App from './App';
+import App from './components/App/App';
 
 export default function render(req, res) {
   const appString = renderToString(<App />);
--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import App from './components/App/App';
 
 render(<App />, document.getElementById('root'));
 if(module.hot) {
