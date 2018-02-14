6e79d938db87001a78519fba455273a762d1fb1d
varunzxzx <varun995862@gmail.com>
2018-02-14 22:28:26 +0530

    trending component

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -2,22 +2,11 @@ import React, { Component } from 'react';
 import Slider from 'react-slick';
 import { Card, Icon, Image, Label, Grid } from 'semantic-ui-react'
 import './Trending.css';
-import {Desktop, Mobile} from '../../helpers/responsive'
 
 const settings = {
     dots: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
-    className: 'slides',
-    adaptiveHeight: true,
-    autoplay: true
-};
-
-const settingss = {
-    dots: true,
-    speed: 500,
-    slidesToShow: 1,
     slidesToScroll: 1,
     className: 'slides',
     adaptiveHeight: true,
@@ -32,97 +21,50 @@ const labels = {
 }
 class Trending extends Component {
     makeTrendingList = () => {
-        return (
-            <div>
-                {Desktop(
-                    <Slider {...settings}>
-                        {[1,0,2,1,3,0].map((i,x) => {
-                            return(
-                                <div key={x}>
-                                    <Card>
-                                        <Card.Content extra>
-                                            <Label size="large" as='a' image>
-                                                <Image avatar spaced='right' src='https://react.semantic-ui.com/assets/images/avatar/small/elliot.jpg' />
-                                                Matthew Stewards
-                                            </Label>
-                                        </Card.Content>
-                                        <Image label={{...labels[i]}} src='https://www.shoutmeloud.com/wp-content/uploads/2017/10/Best-eCommerce-Platforms.jpg' />
-                                        <Card.Content>
-                                            <Card.Header>
-                                                Five Best eCommerce Platforms To Fuel Your Online Business
-                                            </Card.Header>
-                                            <Card.Meta>
-                                                <span className='date'>
-                                                    January 24, 2018
-                                                </span>
-                                            </Card.Meta>
-                                        </Card.Content>
-                                        <Card.Content extra>
-                                            <Grid className="post-footer" columns='equal' divided padded>
-                                                <Grid.Row textAlign='center'>
-                                                    <Grid.Column>
-                                                        <Icon name="unhide" /> 2.2K
-                                                    </Grid.Column>
-                                                    <Grid.Column>
-                                                        <Icon name="heart" /> 663
-                                                    </Grid.Column>
-                                                    <Grid.Column>
-                                                    <Icon name="comments" /> 245
-                                                    </Grid.Column>
-                                                </Grid.Row>
-                                            </Grid>
-                                        </Card.Content>
-                                    </Card>
-                                </div>
-                            )
-                        })}
-                    </Slider>
-                )}
-                {Mobile(
-                    <Slider {...settingss}>
-                        {[1,0,2,1,3,0].map((i,x) => {
-                            return(
-                                <div key={x}>
-                                    <Card>
-                                        <Card.Content extra>
-                                            <Label size="large" as='a' image>
-                                                <Image avatar spaced='right' src='https://react.semantic-ui.com/assets/images/avatar/small/elliot.jpg' />
-                                                Matthew Stewards
-                                            </Label>
-                                        </Card.Content>
-                                        <Image label={{...labels[i]}} src='https://www.shoutmeloud.com/wp-content/uploads/2017/10/Best-eCommerce-Platforms.jpg' />
-                                        <Card.Content>
-                                            <Card.Header>
-                                                Five Best Commerce Platforms To Fuel Your Online Business
-                                            </Card.Header>
-                                            <Card.Meta>
-                                                <span className='date'>
-                                                    January 24, 2018
-                                                </span>
-                                            </Card.Meta>
-                                        </Card.Content>
-                                        <Card.Content extra>
-                                            <Grid className="post-footer" columns='equal' divided padded>
-                                                <Grid.Row textAlign='center'>
-                                                    <Grid.Column>
-                                                        <Icon name="unhide" /> 2.2K
-                                                    </Grid.Column>
-                                                    <Grid.Column>
-                                                        <Icon name="heart" /> 663
-                                                    </Grid.Column>
-                                                    <Grid.Column>
-                                                    <Icon name="comments" /> 245
-                                                    </Grid.Column>
-                                                </Grid.Row>
-                                            </Grid>
-                                        </Card.Content>
-                                    </Card>
-                                </div>
-                            )
-                        })}
-                    </Slider>
-                )}
+        return(
+                <Slider {...settings}>
+                    {[1,0,2,1,3,0].map((i,x) => {
+                        return(
+                            <div key={x}>
+                <Card>
+                <Card.Content extra>
+                <Label size="large" as='a' image>
+                    <Image avatar spaced='right' src='https://react.semantic-ui.com/assets/images/avatar/small/elliot.jpg' />
+                    Matthew Stewards
+                </Label>
+                    </Card.Content>
+                    <Image label={{...labels[i]}} src='https://www.shoutmeloud.com/wp-content/uploads/2017/10/Best-eCommerce-Platforms.jpg' />
+                    <Card.Content>
+                    <Card.Header>
+                        Five Best eCommerce Platforms To Fuel Your Online Business
+                    </Card.Header>
+                    <Card.Meta>
+                        <span className='date'>
+                            January 24, 2018
+                        </span>
+                    </Card.Meta>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <Grid className="post-footer" columns='equal' divided padded>
+                            <Grid.Row textAlign='center'>
+                                <Grid.Column>
+                                    <Icon name="unhide" /> 2.2K
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Icon name="heart" /> 663
+                                </Grid.Column>
+                                <Grid.Column>
+                                <Icon name="comments" /> 245
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Card.Content>
+                </Card>
             </div>
+                    )
+                })}
+            </Slider>
+
         )
     }
     render() {
