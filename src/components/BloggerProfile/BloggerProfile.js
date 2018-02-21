97dbada2221b4c696b4101b556ae696420829bb3
varunzxzx <varun995862@gmail.com>
2018-02-21 20:32:24 +0530

    update blogger profile ui

--- src/components/BloggerProfile/BloggerProfile.js
+++ src/components/BloggerProfile/BloggerProfile.js
@@ -10,7 +10,14 @@ class BloggerProfile extends Component {
                 <Navbar />
                 <Segment className="main" basic>
                     <div className="blogger-profile">
-                        <Image src='https://react.semantic-ui.com/assets/images/avatar/large/elliot.jpg' size='small' circular />
+                        <div>
+                            <Image spaced="right" src='https://react.semantic-ui.com/assets/images/avatar/large/elliot.jpg' size='small' circular/>
+                            <div className="edit"><a href="#">Edit Profile</a></div>
+                        </div>
+                        <div>
+                            <div className="username">Matthew Stewards</div>
+                            <div className="follow-count">0 FOLLOWERS &nbsp;&nbsp; 0 FOLLOWING</div>
+                        </div>
                     </div>
                 </Segment>
                 <Footer />
