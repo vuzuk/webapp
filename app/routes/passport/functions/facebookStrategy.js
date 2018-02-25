0b8d507b673f693c68d14156ac0bbf7ef90e21a3
hackbansu <bansal.pawan96@gmail.com>
2018-02-25 21:38:13 +0530

    updated facebook login

--- app/routes/passport/functions/facebookStrategy.js
+++ app/routes/passport/functions/facebookStrategy.js
@@ -5,7 +5,7 @@ var randomString = require("randomstring");
 let fs = new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://" + process.env.DOMAIN + "/login/facebook/user/callback"
+    callbackURL: "http://" + process.env.DOMAIN + "/facebook/login/callback"
 }, function (accessToken, refreshToken, profile, done) {
     console.log(profile);
     User.findOrCreate({
