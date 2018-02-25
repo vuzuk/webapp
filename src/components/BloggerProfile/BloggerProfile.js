2a3fd1bc18e164118d4fb1dc110df9d8dd00d10d
varunzxzx <varun995862@gmail.com>
2018-02-25 17:08:23 +0530

    add tab navigation in blogger profile

--- src/components/BloggerProfile/BloggerProfile.js
+++ src/components/BloggerProfile/BloggerProfile.js
@@ -4,9 +4,22 @@ import Footer from '../Footer/Footer';
 import { Segment, Image } from 'semantic-ui-react';
 import './BloggerProfile.css';
 class BloggerProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActive: "post"
+        }
+    }
+
+    handleChange = (isActive) => {
+        console.log(isActive);
+        this.setState({isActive});
+    }
+
     render() {
+        const {isActive} = this.state;
         return (
-            <div>
+            <div id="profile-page">
                 <Navbar />
                 <Segment className="main" basic>
                     <div className="blogger-profile">
@@ -20,6 +33,11 @@ class BloggerProfile extends Component {
                         </div>
                     </div>
                 </Segment>
+                <div className="tabs">
+                    <div className="tab" onClick={() => {this.handleChange("post")}} style={isActive === "post" ? {borderBottom: "4px solid #55ACEE"} : null}>POST</div>
+                    <div className="tab" onClick={() => {this.handleChange("likes")}} style={isActive === "likes" ? {borderBottom: "4px solid #55ACEE"} : null}>LIKES</div>
+                    <div className="tab" onClick={() => {this.handleChange("bookmark")}} style={isActive === "bookmark" ? {borderBottom: "4px solid #55ACEE"} : null}>BOOKMARKS</div>
+                </div>
                 <Footer />
             </div>
         )
