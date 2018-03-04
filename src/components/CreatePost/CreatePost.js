0c63741a365db6036b18a995ed36f79386ade0bf
varunzxzx <varun995862@gmail.com>
2018-03-04 15:07:06 +0530

    updateCreate Post

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import { Grid, Input, Segment, Header } from 'semantic-ui-react'
+import './CreatePost.css';
 
 class CreatePost extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                Hey
+                    <Segment basic padded="very">
+                        <Grid>
+                            <Grid.Column width={10}>
+                                <div><span className="subhead">Title</span><Input focus placeholder="Enter title here"/></div>
+                            </Grid.Column>
+                            <Grid.Column width={6}></Grid.Column>
+                        </Grid>
+                    </Segment>
                 <Footer />
             </div>
         )
