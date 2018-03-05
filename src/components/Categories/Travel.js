50ad2864c4708ecbe05acad203ca1d28dd7f7590
varunzxzx <varun995862@gmail.com>
2018-03-05 18:11:19 +0530

    add slider section in travel

--- src/components/Categories/Travel.js
+++ src/components/Categories/Travel.js
@@ -5,6 +5,15 @@ import {Segment, Grid, Header} from 'semantic-ui-react';
 import Slider from 'react-slick';
 import myCard from '../../helpers/card';
 
+const settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: 'slides',
+    autoplay: true
+};
+
 class Travel extends Component {
     render() {
         return (
