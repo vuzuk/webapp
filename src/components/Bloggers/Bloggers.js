c77fa2e1f6357113cd8cddebb15bee97516e7219
varunzxzx <varun995862@gmail.com>
2018-03-05 20:33:35 +0530

    fix bloggers section

--- src/components/Bloggers/Bloggers.js
+++ src/components/Bloggers/Bloggers.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Button, Divider, Header, Image, List } from 'semantic-ui-react'
+import { Segment, Button, Divider, Header, Image, List, Icon } from 'semantic-ui-react'
 import './Bloggers.css';
 import classnames from 'classnames';
 class Bloggers extends Component {
@@ -7,7 +7,7 @@ class Bloggers extends Component {
         return(
             <Segment.Group horizontal className={classnames('bloggers')}>
                 <Segment>
-                    <Header as='h3' textAlign="center">Top Bloggers</Header>
+                    <Header as='h3' textAlign="center" className="top">Top Bloggers</Header>
                     <List selection divided verticalAlign='middle' size="massive" classnames="list">
                         <List.Item>
                             <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/matthew.png' />
@@ -30,24 +30,10 @@ class Bloggers extends Component {
                                 <List.Description>Punjab</List.Description>
                             </List.Content>
                         </List.Item>
-                        <List.Item>
-                            <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/veronika.jpg' />
-                            <List.Content>
-                                <List.Header as='a'>Veronika</List.Header>
-                                <List.Description>West Bengal</List.Description>
-                            </List.Content>
-                        </List.Item>
-                        <List.Item>
-                            <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/joe.jpg' />
-                            <List.Content>
-                                <List.Header as='a'>Joe</List.Header>
-                                <List.Description>Jharkhand</List.Description>
-                            </List.Content>
-                        </List.Item>
                     </List>
                 </Segment> 
             <Segment>
-                <Header as='h3' textAlign="center">New Bloggers</Header>
+                <Header as='h3' textAlign="center" className="new">New Bloggers</Header>
                 <List selection divided verticalAlign='middle' size="massive" classnames="list">
                     <List.Item>
                         <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/matthew.png' />
@@ -68,20 +54,6 @@ class Bloggers extends Component {
                         <List.Content>
                             <List.Header as='a'>Elliot Fu</List.Header>
                             <List.Description>Punjab</List.Description>
-                        </List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/veronika.jpg' />
-                        <List.Content>
-                            <List.Header as='a'>Veronika</List.Header>
-                            <List.Description>West Bengal</List.Description>
-                        </List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <Image avatar src='https://react.semantic-ui.com/assets/images/avatar/small/joe.jpg' />
-                        <List.Content>
-                            <List.Header as='a'>Joe</List.Header>
-                            <List.Description>Jharkhand</List.Description>
                         </List.Content>
                     </List.Item>
                 </List>
