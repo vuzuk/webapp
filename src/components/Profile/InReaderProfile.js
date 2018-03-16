34d68b6d03d8b6c44cb8c65ca1c2265d2f926216
varunzxzx <varun995862@gmail.com>
2018-03-16 16:58:43 +0530

    improve styles in reader profile

--- src/components/Profile/InReaderProfile.js
+++ src/components/Profile/InReaderProfile.js
@@ -26,10 +26,10 @@ class InReaderProfile extends Component {
                     <div className="blogger-profile">
                         <div>
                             <Image spaced="right" src='https://react.semantic-ui.com/assets/images/avatar/large/elliot.jpg' size='small' circular/>
-                            <div className="edit"><a href="#">Edit Profile</a></div>
                         </div>
                         <div>
                             <div className="username">Matthew Stewards</div>
+                            <div className="edit" style={{float: "left"}}><a href="#">Edit Profile</a></div>
                             {/* <div className="follow-count">0 FOLLOWERS &nbsp;&nbsp; 0 FOLLOWING</div> */}
                         </div>
                     </div>
