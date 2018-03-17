3090046940b2988f03f03c70c2ed5ebcadb7807f
varunzxzx <varun995862@gmail.com>
2018-03-17 19:13:21 +0530

    add icons to buttons

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
-import { Grid, Input, Segment, Header, Select, Button } from 'semantic-ui-react'
+import { Grid, Input, Segment, Header, Select, Button, Dimmer, Divider, Icon } from 'semantic-ui-react'
 import 'froala-editor/js/froala_editor.pkgd.min.js';
 import 'froala-editor/js/plugins/video.min.js';
 import 'froala-editor/js/plugins/emoticons.min.js';
@@ -40,6 +40,13 @@ const categoryOptions = [
 ]
 
 class CreatePost extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isDimmed: true
+        }
+    }
     removeWrapper = () => {
         let elements = document.querySelectorAll(".fr-wrapper a[target='_blank'");
         console.log("run..")
@@ -74,14 +81,21 @@ class CreatePost extends Component {
                             <Grid.Column width={10}>
                                 <Input size="big" focus fluid placeholder="Enter title here"/>
                                 <div style={{marginTop: "10px"}}>
-                                    <FroalaEditor config={{
-                                        height: 300,
-                                        placeholderText: 'Edit Your Content Here!',
-                                        imageUploadURL: '/api/upload',
-                                        charCounterCount: false,
-                                        quickInsertButtons: [],
-                                        toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertLink', 'insertImage', 'insertVideo','insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'spellChecker', 'help', 'html', '|', 'undo', 'redo']
-                                    }} />
+                                    <Dimmer.Dimmable dimmed={this.state.isDimmed}>
+                                        <Dimmer active={this.state.isDimmed}>
+                                            <Button primary size="large" icon labelPosition='left'><Icon name='write' /> Create Blog Post</Button>
+                                            <Divider horizontal>OR</Divider>
+                                            <Button size="large" icon labelPosition='left'><Icon name='linkify' /> Submit Post Link</Button>
+                                        </Dimmer>
+                                        <FroalaEditor config={{
+                                            height: 300,
+                                            placeholderText: 'Edit Your Content Here!',
+                                            imageUploadURL: '/api/upload',
+                                            charCounterCount: false,
+                                            quickInsertButtons: [],
+                                            toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertLink', 'insertImage', 'insertVideo','insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'spellChecker', 'help', 'html', '|', 'undo', 'redo']
+                                        }} />
+                                    </Dimmer.Dimmable>
                                 </div>
                             </Grid.Column>
                             <Grid.Column width={6}>
