eb57abe0948c0bbeba2c6356da059d15ea0bbfa4
varunzxzx <varun995862@gmail.com>
2018-03-25 19:31:25 +0530

    update trending ui

--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { Segment, Header, Icon } from 'semantic-ui-react';
+import { Segment, Header, Icon, Label } from 'semantic-ui-react';
+import './Trending.css';
 class Trending extends Component {
+    makeTags = (e) => {
+        return e.map(label => {
+            return (
+                <Label as="a" size="big">
+                    <Icon name='hashtag' /> {label}
+                </Label>
+            )
+        })
+    }
     render() {
         return(
             <div className="trend-tags">
                 <Segment vertical padded>
                     <Header as='h3' className="category-title" icon textAlign="center">
-                    <Icon name='tags' circular/>
+                    <Icon name='hashtag' circular/>
                         TRENDING #tags
                     </Header>
                 </Segment>
+                <div style={{padding: "10px", display: "flex"}}>
+                    {this.makeTags(["delhi","vintage","instafood","malware","foodgasm"])}
+                </div>
             </div>
         )
     }
