1438fb91fad244033e15a883935a3a58cafcb73d
varunzxzx <varun995862@gmail.com>
2018-05-20 18:48:37 +0530

    priortize app bundle

--- src/template.js
+++ src/template.js
@@ -16,9 +16,9 @@ export default ({ body, title }) => {
     </head>
     <body>
       <div id="root">${body}</div>
-      <script type='text/javascript' src='http://platform-api.sharethis.com/js/sharethis.js#property=5abf669cce89f00013641dd1&product=inline-share-buttons' async='async'></script>
       <script src='/vendor.js'></script>
       <script src='/bundle.js'></script>
+      <script type='text/javascript' src='http://platform-api.sharethis.com/js/sharethis.js#property=5abf669cce89f00013641dd1&product=inline-share-buttons' async='async'></script>
     </body>
   </html>
 `;
