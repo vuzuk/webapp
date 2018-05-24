bc822739531ec7fc784274962867cb807bc692c2
varunzxzx <varun995862@gmail.com>
2018-05-24 21:18:21 +0530

    fix css preloading

--- src/template.js
+++ src/template.js
@@ -4,11 +4,11 @@ export default ({ body, title }) => {
   <html>
     <head>
       <title>${title}</title>
-      <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
       <link rel='stylesheet' href='/bundle.css'>
+      <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
       <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
       <link rel="stylesheet" href="/froala_style.min.css">
       <link rel="stylesheet" href="/video.min.css">
       <link rel="stylesheet" href="/emoticons.min.css">
