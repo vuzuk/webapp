42a9326d1c386dccbc225a41e0a853c5dbdb5bbb
varunzxzx <varun995862@gmail.com>
2018-05-24 22:04:52 +0530

    fix css priority

--- src/template.js
+++ src/template.js
@@ -4,9 +4,9 @@ export default ({ body, title }) => {
   <html>
     <head>
       <title>${title}</title>
-      <link rel="stylesheet" href="/froala_editor.pkgd.min.css">
       <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
       <link rel='stylesheet' href='/bundle.css'>
+      <link rel="stylesheet" href="/froala_editor.pkgd.min.css">
       <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
       <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
