73a6cba1cc0b4563ab6a45cc165f6444d52ed19c
varunzxzx <varun995862@gmail.com>
2018-05-26 17:47:35 +0530

    structure hepers

--- src/SSR.js
+++ src/SSR.js
@@ -3,14 +3,16 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter, matchPath } from "react-router-dom"
 import template, {index} from './template';
 import routes from './routes';
+import isEmpty from './helpers/isEmpty';
 
 export default function render(req,res) {
   const activeRoute = routes.find(route => matchPath(req.url, route));
   const {component: Component, title} = activeRoute;
   const context = {};
+  const isLogin = !isEmpty(req.user);
   const appString = renderToString(
     <StaticRouter location={req.url} context={context}>
-      <Component isLogin={true}/>
+      <Component isLogin={isLogin}/>
     </StaticRouter>
   );
   res.send(template({
