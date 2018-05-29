f136f27c3949a06fffdf8062b17f09bcf3123c18
varunzxzx <varun995862@gmail.com>
2018-05-29 22:00:27 +0530

    add related user in post page

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -25,6 +25,39 @@ Why does everyone need mobile devs? Because the web is slowly dying. I have frie
 
 They managed, but it sure as hell wasn’t easy, because Android’s dev stack is the world’s biggest poo sandwich.</p>`
 
+const users = [
+    {
+        name: "Deep Kaur",
+        follower: "9548",
+        img: "https://react.semantic-ui.com/assets/images/avatar/large/helen.jpg"
+    },
+    {
+        name: "Vipul Gupta",
+        follower: "2795",
+        img: "https://react.semantic-ui.com/assets/images/avatar/large/christian.jpg"
+    },
+    {
+        name: "Daniel",
+        follower: "5523",
+        img: "https://react.semantic-ui.com/assets/images/avatar/large/daniel.jpg"
+    },
+    {
+        name: "Deep Kaur",
+        follower: "9548",
+        img: "https://react.semantic-ui.com/assets/images/avatar/large/helen.jpg"
+    },
+    {
+        name: "Vipul Gupta",
+        follower: "2795",
+        img: "https://react.semantic-ui.com/assets/images/avatar/large/christian.jpg"
+    },
+    {
+        name: "Daniel",
+        follower: "5523",
+        img: "https://react.semantic-ui.com/assets/images/avatar/large/daniel.jpg"
+    }
+]
+
 class Post extends Component {
     constructor(props) {
         super(props);
@@ -41,9 +74,9 @@ class Post extends Component {
     makeList = (e) => {
         return e.map(i => {
             return(
-                <List.Item>
-                    {myCard(i)}
-                </List.Item>
+                <Grid.Column key={i}>
+                    {myCard(i,"Matthew")}
+                </Grid.Column>
             )
         })
     }
@@ -117,6 +150,9 @@ class Post extends Component {
                                     </Grid.Column>
                                 </Grid.Row>
                             </Grid>
+                            <Grid columns={3}>
+                                {this.makeList([1,2,3])}
+                            </Grid>
                         </Grid.Column>
                         <Grid.Column width={5}>
                             <Segment className="topics">
@@ -158,11 +194,21 @@ class Post extends Component {
                                     </List.Item>
                                 </List>
                             </Segment>
-                            <div style={{paddingLeft: "10%", paddingTop: "4%", paddingBottom: "4%"}}>
-                                <List relaxed>
-                                    {this.makeList([1,2,3])}
+                            <Segment>
+                                <Header>Related Users</Header>
+                                <List size="big" divided relaxed>
+                                    {users.map(user => (<List.Item>
+                                    <Image avatar src={user.img} size="tiny"/>
+                                    <List.Content>
+                                        <List.Header>{user.name}</List.Header>
+                                        <div style={{fontSize: "0.8em", marginTop: "10px"}}><a style={{display: "block"}} href="#">@{user.name.split(" ")[0].toLowerCase()}</a>{user.follower} Followers</div>
+                                    </List.Content>
+                                    <List.Content floated='right'>
+                                        <Button>Follow</Button>
+                                    </List.Content>
+                                    </List.Item>))}
                                 </List>
-                            </div>
+                            </Segment>
                         </Grid.Column>
                     </Grid>
                 </div>
