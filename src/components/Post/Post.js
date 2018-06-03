5c29a48672b8587922855eb1dc8275d8a8105e44
varunzxzx <varun995862@gmail.com>
2018-06-03 17:41:00 +0530

    add comment

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -47,7 +47,8 @@ class Post extends Component {
             isActive: 'popular',
             data: props.data,
             customData: props.customData[0],
-            isLiked: false
+            isLiked: false,
+            comment: ""
         }
     }
 
@@ -63,6 +64,31 @@ class Post extends Component {
                 </Grid.Column>
             )
         })
+    }
+
+    addComment = (e, x, parentId) => {
+        const { comment: text, customData } = this.state;
+        
+        const data = {
+            comment: text,
+            blogId: customData.blogs[0].id,
+            parentId
+        }
+        console.log(data);
+        
+        axios({
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            url: '/api/secure/generic/addComment',
+            data: JSON.stringify(data)
+        })
+        .then((res) => {
+            console.log(res);
+            
+        })
+        .catch(err => console.log(err))
     }
 
     toggleLike = () => {
@@ -215,73 +241,28 @@ class Post extends Component {
                 <Divider />
                     <Comment.Group size="large">
                         <Header as='h2'>Comments</Header>
-
-                        <Comment>
-                        <Comment.Avatar src='https://react.semantic-ui.com/assets/images/avatar/small/matt.jpg' />
-                        <Comment.Content>
-                            <Comment.Author as='a'>Matt</Comment.Author>
-                            <Comment.Metadata>
-                            <div>Today at 5:42PM</div>
-                            </Comment.Metadata>
-                            <Comment.Text>How artistic!</Comment.Text>
-                            <Comment.Actions>
-                            <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                        </Comment>
-
-                        <Comment>
-                        <Comment.Avatar src='https://react.semantic-ui.com/assets/images/avatar/small/elliot.jpg' />
-                        <Comment.Content>
-                            <Comment.Author as='a'>Elliot Fu</Comment.Author>
-                            <Comment.Metadata>
-                            <div>Yesterday at 12:30AM</div>
-                            </Comment.Metadata>
-                            <Comment.Text>
-                            <p>This has been very useful for my research. Thanks as well!</p>
-                            </Comment.Text>
-                            <Comment.Actions>
-                            <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                        <Comment.Group size="large">
-                            <Comment>
-                            <Comment.Avatar src='https://react.semantic-ui.com/assets/images/avatar/small/jenny.jpg' />
-                            <Comment.Content>
-                                <Comment.Author as='a'>Jenny Hess</Comment.Author>
-                                <Comment.Metadata>
-                                <div>Just now</div>
-                                </Comment.Metadata>
-                                <Comment.Text>
-                                Elliot you are always so right :)
-                                </Comment.Text>
-                                <Comment.Actions>
-                                <Comment.Action>Reply</Comment.Action>
-                                </Comment.Actions>
-                            </Comment.Content>
-                            </Comment>
-                        </Comment.Group>
-                        </Comment>
-
-                        <Comment>
-                        <Comment.Avatar src='https://react.semantic-ui.com/assets/images/avatar/small/joe.jpg' />
-                        <Comment.Content>
-                            <Comment.Author as='a'>Joe Henderson</Comment.Author>
-                            <Comment.Metadata>
-                            <div>5 days ago</div>
-                            </Comment.Metadata>
-                            <Comment.Text>
-                            Dude, this is awesome. Thanks so much
-                            </Comment.Text>
-                            <Comment.Actions>
-                            <Comment.Action>Reply</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                        </Comment>
-
+                        {
+                            customData.blogs[0].comments.map(comment => (
+                                <Comment>
+                                <Comment.Avatar src='https://react.semantic-ui.com/assets/images/avatar/small/joe.jpg' />
+                                <Comment.Content>
+                                    <Comment.Author as='a'>Joe Henderson</Comment.Author>
+                                    <Comment.Metadata>
+                                    <div>5 days ago</div>
+                                    </Comment.Metadata>
+                                    <Comment.Text>
+                                        Dude, this is awesome. Thanks so much
+                                    </Comment.Text>
+                                    <Comment.Actions>
+                                    <Comment.Action>Reply</Comment.Action>
+                                    </Comment.Actions>
+                                </Comment.Content>
+                                </Comment>
+                            ))
+                        }
                         <Form reply>
-                        <Form.TextArea />
-                        <Button content='Add Comment' labelPosition='left' icon='edit' primary />
+                        <Form.TextArea onChange={(e) => {this.setState({comment: e.target.value})}} placeholder="Type your comment here..."/>
+                            <Button onClick={this.addComment} content='Add Comment' labelPosition='left' icon='edit' primary />
                         </Form>
                     </Comment.Group>
                 </div>
