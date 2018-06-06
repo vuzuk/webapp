8ac8c390dbb6881f5f5f3e4966d4fd03a4440d28
hackbansu <bansal.pawan96@gmail.com>
2018-06-06 17:04:13 +0530

    updated notifications

--- app/routes/secure/generic/functions/getNotifications.js
+++ app/routes/secure/generic/functions/getNotifications.js
@@ -1,12 +1,13 @@
 const Op = require("sequelize").Op;
 const models = require(process.env.APP_ROOT + "/app/db/models");
+const Blog = models["blog"];
 const Blogger = models["blogger"];
 const User = models["user"];
 const Notification = models['notification']
 
 module.exports = (req, res) => {
     let whereObj = {};
-    whereObj[req['user']['isBlogger'] ? 'b_user_id': 'user_id'] = req['user']['id'];
+    whereObj[req['user']['isBlogger'] ? 'b_user_id' : 'user_id'] = req['user']['id'];
     Notification
         .findAll({
             where: whereObj,
@@ -20,7 +21,44 @@ module.exports = (req, res) => {
                     logging: false
                 })
                 .then(() => {
-                    return res.status(200).json({status: true, msg: notifications});
+                    let myNotifications = notifications.map((notif) => notif.get({plain: true}));
+                    let blogIds = myNotifications.map((obj) => obj['blog_id'])
+                    Blog
+                        .findAll({
+                            attributes: ["id", "title", "images", "date_published", "views", "slug", "blogger_id", "likes"],
+                            where: {
+                                id: blogIds
+                            },
+                            include: [{
+                                model: Comment,
+                                attributes: ['comment'],
+                            }, {
+                                model: Blogger,
+                                attributes: ['username', 'first_name', 'last_name', 'image']
+                            }]
+                        })
+                        .then((blogs) => {
+                            if (blogs.length === 0) {
+                                return res.status(400).json({status: true, msg: "not found"});
+                            }
+
+                            const myBlogs = blogs.map((blog) => blog.get({plain: true}));
+                            for (let i = 0; i < myBlogs.length; i++) {
+                                myBlogs[i]['comments'] = myBlogs[i]['comments'].length;
+                            }
+
+                            for(let i =0; i<myBlogs.length; i++){
+                                myNotifications[i]['blog'] = myBlogs[i]
+                            }
+
+                            // return render.default(req, res, {status: true, msg: blog})
+                            return res.status(200).json({status: true, msg: myBlogs});
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            return res.status(503).json({status: false, msg: "error in database"})
+                        });
+                    return res.status(200).json({status: true, msg: myNotifications});
                 })
         })
         .catch((err) => {
