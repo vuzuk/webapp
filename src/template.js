91b06679ea71a3bfdb110459322556fa0ba2d5d2
varunzxzx <varun995862@gmail.com>
2018-07-17 22:09:33 +0530

    add facebook pixel

--- src/template.js
+++ src/template.js
@@ -26,6 +26,23 @@ export function staticHTML ({ title, data, customData }) {
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
       <link rel="stylesheet" href="/froala_style.min.css">
       <link rel="stylesheet" href="/video.min.css">
+      <!-- Facebook Pixel Code -->
+      <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1012523192248394');
+        fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=1012523192248394&ev=PageView&noscript=1"
+      /></noscript>
+      <!-- End Facebook Pixel Code -->
     </head>
     <body>
 `;
